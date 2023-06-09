14d6fea8b854e1c40706b28338e7634b5166a44e
mergezy <azgardex9998@gmail.com>
2023-06-09 08:01:23 +0300

    u

---
+++ app/src/main/java/com/example/activity/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.activity.;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private static final String KEY_COUNT = "counter";
+    public static final String TAG = "StartActivity";
+    private Integer counter = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_COUNT, counter);
+        Log.d(TAG, "onSaveInstanceState");
+        Toast.makeText(this, "onSaveInstanceState()", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        counter = savedInstanceState.getInt(KEY_COUNT);
+        TextView text = findViewById(R.id.txt_counter);
+        text.setText(counter.toString());
+        Log.d(TAG, "onRestoreInstanceState");
+        Toast.makeText(this, "onRestoreInstanceState()", Toast.LENGTH_LONG).show();
+    }
+    public void Click (View view){
+        counter++;
+        TextView text = (TextView)findViewById(R.id.txt_counter);
+        text.setText(counter.toString());
+    }
+    public void onClickButton (View view){
+        counter = 0;
+        TextView text = findViewById(R.id.txt_counter);
+        text.setText(counter.toString());
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        resetUI();
+        Toast.makeText(this, "onStart()", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        Toast.makeText(this, "onResume()", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+        Toast.makeText(this, "onPause()", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+        Toast.makeText(this, "onStop()", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+        Toast.makeText(this, "onRestart()", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+        Toast.makeText(this, "onDestroy()", Toast.LENGTH_LONG).show();
+    }
+    private void resetUI() {
+        ((TextView) findViewById(R.id.txt_counter)).setText(count.toString());
+        Log.d(TAG, "resetUI");
+    }
+}
